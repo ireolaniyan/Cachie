403383914e65e6293383f8ce322f669ca7a828e1
Ire Olaniyan <ireolaniyan7@gmail.com>
2022-08-26 20:38:14 +0100

    chore:
    - add jest config file
    - add dummy test
    - validate search route using Joi

--- src/routes/cachie.ts
+++ src/routes/cachie.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import * as CachieContoller from '../controllers/cachie';
+import * as CachieValidation from '../validations/cachie';
 
 const router = express.Router();
 
-router.post('/search', CachieContoller.search);
+router.post(
+  "/search",
+  CachieValidation.validateSearchCachie,
+  CachieContoller.search
+);
 
 export default router;
---
+++ src/validations/cachie.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+export async function validateSearchCachie(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<Response | void> {
+  const schema = Joi.object()
+    .keys({
+      search_query: Joi.string().required(),
+    })
+    .unknown(false);
+
+  const validation = schema.validate(req.body);
+
+  if (validation.error) {
+    const error = validation.error.message
+      ? validation.error.message
+      : validation.error.details[0].message;
+
+    return res.status(400).send({
+      error,
+    });
+  }
+
+  return next();
+}
